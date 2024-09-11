14a69980529c49c575021f1c9415ca0a4d69fcff
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-09-12 00:02:21 +0200

    Update info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -10,27 +10,9 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   const tradutor = _translate.plugins.info_donar
 
   const name = await conn.getName(m.sender);
-  const donar =`
-*┏ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*
-*┇          「 ${tradutor.texto1[0]} 」*
-*┣ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*
-*┃ ${tradutor.texto1[1]} ${name}*
-*┃*
-*┃ ${tradutor.texto1[2]}*
-*┃ ${tradutor.texto1[3]}*
-*┃*
-*┃ ${tradutor.texto1[4]}* 
-*┃ ${tradutor.texto1[5]}*
-*┃ ${tradutor.texto1[6]}*
-*┃ ${tradutor.texto1[7]}* 
-*┃ ${tradutor.texto1[8]}*  
-*┃ ${tradutor.texto1[9]}* 
-*┃*
-*┃ ${tradutor.texto1[10]}* 
-*┃ ${tradutor.texto1[11]}* 
-*┃ ${tradutor.texto1[12]}* 
-*┗ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*
-`.trim();
+  const donar =`Hola si quieres donar a este bot el paypal es:
+  https://paypal.me/Richetti123
+  Puedes mandar captura de tu donacion al wa.me/56928561853`.trim();
   const aa = {quoted: m, userJid: conn.user.jid};
   const res = generateWAMessageFromContent(m.chat, {liveLocationMessage: {degreesLatitude: 0, degreesLongitude: 0, caption: donar, secuenceNumber: '0', contextInfo: {mentionedJid: conn.parseMention()}}}, aa);
   conn.relayMessage(m.chat, res.message, {});
