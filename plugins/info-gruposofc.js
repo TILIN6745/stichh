a54c85046b4dc270c4efa394843b46c9aee5225b
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-09-12 00:07:00 +0200

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -6,24 +6,13 @@ const handler = async (m, {conn, usedPrefix}) => {
 
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `${tradutor.texto1[0]}
+  const text = `BIENVENIDO A LOS GRUPOS OFICIALES DE ${global.packname}
+https://chat.whatsapp.com/LfJfgWkF25pIik8KW0QPU6
 
-${tradutor.texto1[1]}
-1.- https://chat.whatsapp.com/LjJbmdO0qSDEKgB60qivZj
-
-2.- https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs
-
-3.- https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F
-
-4.- https://chat.whatsapp.com/CjexkGVr37J6GuSdDVAHzC
-
-> Sunlight Team  :
-
-1.- https://whatsapp.com/channel/0029Vam7yUg77qVaz3sIAp0z
-
-2.- https://chat.whatsapp.com/Fy74b6fgE9SJJpHVi6CKJY`.trim();
+CANAL OFICIAL
+https://whatsapp.com/channel/0029VaZ90V9EKyZGyqHKt61M`.trim();
   const buttonMessage= {
-    'document': {url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`},
+    'document': {url: md},
     'mimetype': `application/${document}`,
     'fileName': `${tradutor.texto2}`,
     'fileLength': 99999999999999,
@@ -32,13 +21,13 @@ ${tradutor.texto1[1]}
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://github.com/BrunoSobrino/TheMystic-Bot-MD',
+        'mediaUrl': md,
         'mediaType': 2,
         'previewType': 'pdf',
-        'title': `${tradutor.texto3}`,
+        'title': wm,
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'}},
+        'sourceUrl': md}},
     'caption': text,
     'footer': wm,
     'headerType': 6};
