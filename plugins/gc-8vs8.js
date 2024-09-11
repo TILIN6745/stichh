1f9a0b6545102068a739d1c4983eef4c36a14f22
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-09-12 00:52:08 +0200

    Update gc-8vs8.js

--- plugins/gc-8vs8.js
+++ plugins/gc-8vs8.js
@@ -7,7 +7,33 @@ const handler = async (m, { conn, args, command, usedPrefix }) => {
   const tradutor = _translate.plugins.descargas_instagram;
 
   if (!args[0])
-    throw `${tradutor.texto1} _${usedPrefix + command} https://www.instagram.com/reel/C8sWV3Nx_GZ/?igsh=MWZoeTY2cW01Nzg1bQ==`;
+    throw `8 𝐕𝐄𝐑𝐒𝐔𝐒 8
+
+𝐇𝐎𝐑𝐀𝐑𝐈𝐎
+🇵🇪 𝐏𝐄𝐑𝐔 : 
+🇦🇷 𝐀𝐑𝐆𝐄𝐍𝐓𝐈𝐍𝐀 : 
+🇨🇱 𝐂𝐇𝐈𝐋𝐄 :  
+𝐂𝐎𝐋𝐎𝐑 𝐃𝐄 𝐑𝐎𝐏𝐀: 
+¬ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒 𝐏𝐑𝐄𝐒𝐄𝐍𝐓𝐄𝐒
+
+      𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 1
+
+👑 ┇ 
+🥷🏻 ┇  
+🥷🏻 ┇ 
+🥷🏻 ┇ 
+      
+     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 2
+
+👑 ┇ 
+🥷🏻 ┇ 
+🥷🏻 ┇ 
+🥷🏻 ┇ 
+
+ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+🥷🏻 ┇ 
+🥷🏻 ┇
+`;
   await m.reply(global.wait);
   try {
     const img = await instagramDownload(args[0]);
