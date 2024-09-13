8872825a5070077d1ffbfc1808be7c60cdcfb3e6
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-09-13 22:04:19 +0200

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -17,7 +17,7 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   for (const mem of participants) {
     teks += `☀️✨≋ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `MichiBot`;
+  teks += `${global.packaname}`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
