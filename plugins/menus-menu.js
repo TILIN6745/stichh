8e6bdaa31fe90a27686db6b9725f9698b4b0bd3d
Tilinn <jm293229@gmail.com>
2024-10-13 14:06:29 -0600

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -33,7 +33,7 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `*¡HOLA! ${taguser}* 
-*BIENVENIDO AL MENU DE 𝐊𝐄𝐑𝐎𝐏𝐏𝐈*
+*BIENVENIDO AL MENU DE Thoru Bot*
 
 *··················································*
 
